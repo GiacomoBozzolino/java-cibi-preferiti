d9c8b9452b9f755f8a8c1ebe0928285822f2491c
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-09 17:46:40 +0100

    fine esercizio con bonus

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -2,6 +2,18 @@ package org.lessons.java.array;
 
 public class CibiPreferiti {
 	public static void main (String[] Args) {
+		String [] favoriteFoods = {"gelato", "pasta", "pizza", "risotto", "cioccolato", "prosciutto"};
+		
+		System.out.println("Ho"+ " " + favoriteFoods.length + " " + "cibi preferiti.");
+		
+		System.out.println("Il mio cibo preferito in assoluto è il"  + " " + favoriteFoods[0]);
+		
+		System.out.println("Il mio cibo preferito, ma meno degli altri è il" + " " + favoriteFoods[favoriteFoods.length - 1]);
+		
+//		Bonus: Stampate a video anche il cibo di mezza classifica
+		
+		int intAvarageFood = favoriteFoods.length / 2;
+        System.out.println("Il cibo di mezza classifica è il" + " " + favoriteFoods[intAvarageFood]);
 		
 	}
 
